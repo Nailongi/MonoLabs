360b860274c42cca51e7e23fba83790b447a7fc8
elgordomike <wachulacion@gmail.com>
2021-07-15 18:30:49 -0400

    ulaalaaa que cambios

---
+++ Assets/BackgroundObjectManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundObjectManager : MonoBehaviour
+{
+    public GameObject _arbolPixel1;
+    public GameObject _arbolPixel2;
+    public GameObject _nubePixel1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BackrgoundWave());
+    }
+
+    IEnumerator BackrgoundWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            spawn();
+        }
+    }
+
+    private void spawn()
+    {
+        int number = Random.Range(0, 100);
+        if (number >= 35 && number < 85)
+        {
+            GameObject backgroundObject = Instantiate(_arbolPixel1) as GameObject;
+        }
+        else if (number >= 85 && number < 90)
+        {
+            GameObject backgroundObject = Instantiate(_arbolPixel2) as GameObject;
+        }
+        else if (number >= 90 && number < 95)
+        {
+            GameObject backgroundObject = Instantiate(_nubePixel1) as GameObject;
+            backgroundObject.transform.position = new Vector3(backgroundObject.transform.position.x, Random.Range(1.2f, 1.5f));
+        }
+        else
+            new WaitForSeconds(1f);
+    }
+}
