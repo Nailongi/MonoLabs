83baa97a366c8c005fd96405450997fb33db81af
Nailongi <xxdaloxx@gmail.com>
2021-08-29 18:11:33 -0400

    BeeMoves
    *Se mejora el script de movimiento de las abejas

--- Assets/Scripts/EnemyMoves.cs
+++ Assets/Scripts/EnemyMoves.cs
@@ -8,12 +8,16 @@ public class EnemyMoves : MonoBehaviour
     public float speed;
     public bool isABee = false;
     private float Time0;
+    private float y0;
     public float amplitude;
     public float frequency;
+    private float rdmfase;
 
     void Start()
     {
+        y0 = transform.position.y;
         Time0 = Time.time;
+        rdmfase = Random.Range(0f,2*Mathf.PI);
     }
 
     // Update is called once per frame
@@ -22,7 +26,7 @@ public class EnemyMoves : MonoBehaviour
         MoveLeft();
         if (isABee) 
         { 
-            MoveUpDown();
+            MoveUpDown3();
         }
     }
 
@@ -31,8 +35,25 @@ public class EnemyMoves : MonoBehaviour
         transform.Translate(Vector2.left * Time.deltaTime * speed);
     }
 
-    private void MoveUpDown()
+    private void MoveUpDown1()
     {
         transform.position = transform.position + Vector3.up * amplitude * Mathf.Sign(Mathf.Cos((Time0-Time.time) * frequency));
     }
+
+    private float Sawtooth(float t)
+    {
+        return Mathf.Abs(t - Mathf.Floor(t) - 0.5f) - 0.25f;
+    }
+
+    private void MoveUpDown2()
+    {
+        transform.position = new Vector3(transform.position.x, y0 + amplitude * Sawtooth(Time.time * frequency+rdmfase), transform.position.z);
+    }
+
+    private void MoveUpDown3()
+    {
+        transform.position = new Vector3(transform.position.x, y0 + amplitude * Mathf.Cos(Time.time * frequency + rdmfase), transform.position.z);
+    }
+
+    
 }
